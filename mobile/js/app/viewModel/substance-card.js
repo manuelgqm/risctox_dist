05e877be9f2a6159cbe13e6dcc6313546d347081
Lolo <exit>
2016-08-23 15:06:11 +0200

    load fields from viewModel

--- mobile/js/app/viewModel/substance-card.js
+++ mobile/js/app/viewModel/substance-card.js
@@ -2,6 +2,7 @@ define(function(){
 	return {
 		name: 'formaldehído',
 		synonyms: 'formaldehído (concentracion 90 por 100), formaldehído . . . %, formol',
+		cas: '50-00-0',
 		descriptions: {
 			synonyms: 'Se han incluido otros nombres encontrados en la normativa o en las bases de datos utilizadas para construir la RISCTOX',
 			cas: 'Número asignado por el Chemical Abstract Service. Es el sistema de identificación más utilizado a nivel internacional'
