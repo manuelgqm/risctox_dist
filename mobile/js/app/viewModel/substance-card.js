d5f02183ce08c2b47384e62c95befba36969aaa6
Lolo <exit>
2016-08-24 12:10:01 +0200

    load fields from viewModel

--- mobile/js/app/viewModel/substance-card.js
+++ mobile/js/app/viewModel/substance-card.js
@@ -3,9 +3,15 @@ define(function(){
 		name: 'formaldehído',
 		synonyms: 'formaldehído (concentracion 90 por 100), formaldehído . . . %, formol',
 		cas: '50-00-0',
+		einecs: '200-001-8',
+		indice: '605-001-00-5',
+		listaNegra: ['cancerígena', 'mutágena', 'neurotóxica', 'sensibilizante'],
 		descriptions: {
 			synonyms: 'Se han incluido otros nombres encontrados en la normativa o en las bases de datos utilizadas para construir la RISCTOX',
-			cas: 'Número asignado por el Chemical Abstract Service. Es el sistema de identificación más utilizado a nivel internacional'
+			cas: 'Número asignado por el Chemical Abstract Service. Es el sistema de identificación más utilizado a nivel internacional',
+			einecs: 'Catálogo europeo de sustancias químicas comercializadas. Dicho inventario establece la lista definitiva de todas las sustancias que en principio se encontraban en el mercado comunitario al 18 de septiembre de 1981.',
+			indice: 'El número índice es el número de identificación asignado a la sustancia en el anexo VI del Reglamento 1272/2008 (conocido como CLP) de clasificación, etiquetado y envasado de sustancias y mezclas peligrosas',
+			listaNegra: 'La lista negra incluye aquellas sustancias cuyos posibles daños a la salud y al medio ambiente son tan importantes que debemos evitar su uso o presencia en los lugares de trabajo y su vertido al medio ambiente. Estas sustancias, cuya eliminación será prioritaria, son: Cancerígenas, Mutágenas, Tóxicas para la Reproducción, Disruptores Endocrinos, Sensibilizantes, Neurotóxicos, Tóxicas, Persistentes y Bioacumulativas, muy persistent y muy bioacumulativas, Que pueden provocar a largo plazo efectos negativos en el medio ambiente acuático'
 		}
 	}
 })
