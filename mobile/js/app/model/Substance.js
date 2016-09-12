09a986e56d24aa6500da7e6ff5306f1e1588e3c8
Lolo <exit>
2016-09-12 15:23:21 +0200

    new substance with id

--- mobile/js/app/model/Substance.js
+++ mobile/js/app/model/Substance.js
@@ -1,4 +1,4 @@
-define(function(){
+define(['Server'], function(Server){
 	return function(id){
 		this.id = id;
 	}
--- mobile/js/spec/Substance.js
+++ mobile/js/spec/Substance.js
@@ -2,7 +2,8 @@ define(['app/model/Substance'], function(Substance){
 	describe("A substance", function(){
 		it("Must have a id", function(){
 			var substance = new Substance(957597);
-			expect(substance.id).toEqual();
+			expect(substance.id).toEqual(957597);
 		});
+
 	});
 });
