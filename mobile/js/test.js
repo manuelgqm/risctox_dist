de9e9fa320e7bec29c848a44517f4f5b3c7db5a7
Lolo <exit>
2016-09-12 15:26:18 +0200

    add jasmine support for ajax

--- mobile/js/test.js
+++ mobile/js/test.js
@@ -1,20 +1,23 @@
 require.config({
 	baseUrl: 'js/lib',
 	paths: {
+		'spec': '../spec',
+		'app': '../app',
 		'jasmine': ['jasmine-2.5.0/jasmine'],
 		'jasmine-html': ['jasmine-2.5.0/jasmine-html'],
 		'jasmine-boot': ['jasmine-2.5.0/boot'],
-		'spec': '../spec',
-		'app': '../app'
+		'jasmine-ajax': ['jasmine-2.5.0/mock-ajax'],
+		'jquery': 'jquery-3.1.0.min'
 	},
 	shim: {
 		'jasmine-html': {deps : ['jasmine', 'css!jasmine-2.5.0/jasmine.css']},
+		'jasmine-ajax': {deps: ['jasmine']},
 		'jasmine-boot': {deps : ['jasmine', 'jasmine-html']}
 	},
 	map: {'*': {'css' : 'require-css/css.min'}}
 });
 define(['jasmine-boot'], function(){
-	require(['spec/Substance'], function(){
+	require(['spec/substance'], function(){
 		window.onload();
 	});
 });
