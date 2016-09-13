6aa934c80bccd5a9f11f7ac5e5f607362d0affed
Lolo <exit>
2016-09-13 11:09:47 +0200

    change substanceCard model to formolSubstance model

--- mobile/js/app/viewModel/substanceCard.js
+++ mobile/js/app/viewModel/substanceCard.js
@@ -6,12 +6,12 @@ define([
 	'app/model/Substance',
 	'app/viewModel/page',
 	'css!app/view/style/substanceCard'
-], function(_, ViewModel, substanceCardView, substanceCardModel, SubstanceModel, pageViewModel){
+], function(_, ViewModel, substanceCardView, formolSubstance, SubstanceModel, pageViewModel){
 	var substanceCardViewModel = {domId: 'card'};
 	var substanceId = 957597;
 	var substance = new SubstanceModel(substanceId);
 	_.assign(substanceCardViewModel, 
-		substanceCardModel, 
+		formolSubstance, 
 		new ViewModel(substanceCardViewModel, substanceCardView)
 	);
 
