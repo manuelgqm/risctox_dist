cfb94d203af8a413c46b84d5a692f7f88b6acdc5
Lolo <exit>
2016-09-19 16:44:52 +0200

    load pictogramsRd1272 from db

--- lib/db/substancesRepository.asp
+++ lib/db/substancesRepository.asp
@@ -6,7 +6,7 @@
 <!--#include file="substanceApplicationsRepository.asp"-->
 <!--#include file="substanceCompaniesRepository.asp"-->
 <%
-function findSubstance( id_sustancia, connection )
+function findSubstance(id_sustancia, connection)
 	sql = composeSubstanceQuery( id_sustancia )
 	set substanceRecordset = connection.execute(sql)
 	set substance = extractSubstance(id_sustancia, substanceRecordset, connection)
@@ -418,4 +418,62 @@ sub printSusbtance(substance)
 		response.write "<br>"
 	next
 end sub
+
+function getPictogramasRd(simbolosRdString, connection)
+	Dim simbolosRd, i, simbolo, pictogram
+	Dim pictograms : pictograms = Array()
+	
+	simbolosRdString = removeTailSeparator(simbolosRdString, ",")
+	simbolosRd = split(simbolosRdString, ",")
+	for i = 0 to ubound(simbolosRd)
+		simbolo = trim(simbolosRd(i))
+		Set pictogram = findPictogram(simbolo, connection)
+		pictograms = arrayPushDictionary(pictograms, pictogram)
+	next
+	
+	getPictogramasRd = pictograms
+End function
+
+function findPictogram(simbolo, connection)
+	dim sql, simbolosRecodset
+	dim pictogram : Set pictogram = Server.CreateObject("Scripting.Dictionary")
+	simbolo = trim(simbolo)
+	
+	sql = "SELECT imagen, descripcion FROM dn_simbolos WHERE simbolo='" & simbolo & "' and simbolo like 'GHS%'"
+	set simbolosRecodset = connection.execute(sql)
+	if simbolosRecodset.eof then
+		pictogram.add "name", simbolo
+		pictogram.add "image", ""
+		pictogram.add "description", simbolo
+		Set findPictogram = pictogram
+		exit function
+	end if
+	pictogram.add "name", simbolo
+	pictogram.add "image", simbolosRecodset("imagen").value
+	pictogram.add "description", simbolosRecodset("descripcion").value
+
+	simbolosRecodset.close()
+	set simbolosRecodset = nothing
+	Set findPictogram = pictogram
+end function
+
+function decidePictogramDescription(image, description, simbolo)
+	if image = "" Then
+		decidePictogramDescription = simbolo
+		Exit function
+	end if
+
+	decidePictogramDescription = description
+end function
+
+function removeTailSeparator(str, separator)
+	dim result
+
+	result = str
+	if Right(str, 1) = separator then
+		result = Left(str, Len(str) - 1)
+	end if
+
+	removeTailSeparator = result
+end function
 %>
--- mobile/js/app/model/Substance.js
+++ mobile/js/app/model/Substance.js
@@ -12,19 +12,6 @@ define(['lodash', 'Server', 'stringExtended'], function(_, Server){
 				clp: 'Incluye las indicaciones de peligro (frases H) asignadas a las sustancias incluidas en el Anexo VI del Reglamento 1272/2008 (CLP). Los grupos de sustancias incluidos en el Reglamento 1272/2008, Ej. compuestos de berilio, se han desglosado en la relación de sustancias que forman este grupo y se les han asignado las frases H y los pictogramas y palabras de advertencia pertenecientes al grupo.	Las sustancias que pertenecen a 2 grupos, Ej. cromato de mercurio, se han clasificado asignando las frases de mayor riesgo de cada grupo o sus combinaciones, según los criterios del Reglamento CLP.',
 				vla_ec: 'Es el valor de referencia para la Exposición de Corta Duración (EC), que es la concentración media del agente químico en la zona de respiración del trabajador, medida o calculada para cualquier período de 15 minutos a lo largo de la jornada laboral, excepto para aquellos agentes químicos para los que se especifique un período de referencia inferior, en la lista de Valores Límite.Lo habitual es determinar las EC de interés, es decir, las del período o períodos de máxima exposición, tomando muestras de 15 minutos de duración en cada uno de ellos. El VLA-EC no debe ser superado por ninguna EC a lo largo de la jornada laboral.Para aquellos agentes químicos que tienen efectos agudos reconocidos pero cuyos principales efectos tóxicos son de naturaleza crónica, el VLA-EC constituye un complemento del VLA-ED y, por tanto, la exposición a estos agentes habrá de valorarse en relación con ambos límites.En cambio, a los agentes químicos de efectos principalmente agudos como, por ejemplo, los gases irritantes, sólo se les asigna para su valoración un VLA-EC.'
 			},
-			pictograms: [{
-				name: 'Toxicidad crónica',
-				iconUrl: '../imagenes/pictogramas/pictograma_sensibilizacion_respiratoria.png'
-			}, {
-				name: 'Toxicidad aguda (oral, cutánea, por inhalación)',
-				iconUrl: '../imagenes/pictogramas/pictograma_toxicidad_aguda.png'
-			}, {
-				name: 'Corrosivo',
-				iconUrl: '../imagenes/pictogramas/pictograma_corrosivo_metales.png'
-			}, {
-				name: 'Peligro',
-				iconUrl: '../imagenes/pictogramas/pictograma_peligro.png'
-			}],
 			frasesH: [
 				{value: 'H350', description: 'Puede provocar cáncer'}, 
 				{value: 'H341', description: 'Se sospecha que provoca defectos genéticos'}, 
@@ -66,6 +53,16 @@ define(['lodash', 'Server', 'stringExtended'], function(_, Server){
 			this.nombre = this.nombre.decodeHtmlEntity();
 			this.sinonimos = this.sinonimos.map( element => element.decodeHtmlEntity());
 		};
+
+		this.getPictogramRdImageUrl = function(image, simbolo){
+			const PICTOGRAMS_IMAGES_BASE_PATH = '../imagenes/pictogramas/';
+			const PELIGRO_IMAGE = "pictograma_peligro.gif"
+			const ATENCION_IMAGE = "pictograma_atencion.gif"
+			if (simbolo == "Peligro") return PICTOGRAMS_IMAGES_BASE_PATH + PELIGRO_IMAGE;
+			if (simbolo == "Atención") return PICTOGRAMS_IMAGES_BASE_PATH + ATENCION_IMAGE;
+			return PICTOGRAMS_IMAGES_BASE_PATH + image;
+		};
+
 	}
 
 });
