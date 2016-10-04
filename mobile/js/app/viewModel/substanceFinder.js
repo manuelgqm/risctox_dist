a07f40ea4cc35e7af9bb0ba5101a3198beada904
Lolo <exit>
2016-10-04 15:36:25 +0200

    add form behaviour

--- mobile/js/app/viewModel/substanceFinder.js
+++ mobile/js/app/viewModel/substanceFinder.js
@@ -1,12 +1,32 @@
 define([
+	'knockout',
 	'app/viewModel/ViewModel',
 	'text!app/view/substanceFinder.html', 
-	'app/model/substanceFinder'
-], function(ViewModel, substanceFinderView, substanceFinderModel){
-	var substanceFinderViewModel = {domId: 'finder'};
-	Object.assign(substanceFinderViewModel, 
-		substanceFinderModel,
-		new ViewModel(substanceFinderViewModel, substanceFinderView)
-	);
-	return substanceFinderViewModel;
+], function(ko, ViewModel, view){
+	var module = {
+		run: function(){
+			var Finder = function(domId){
+				this.domId = domId;
+				this.name = ko.observable('');
+				this.code = ko.observable('');
+				this.runFinder = function () {
+					var doSearch = () => window.location = "#/search/" + this.name() + "/" + this.code();
+					var validate = () => this.name().length + this.code().length != 0;
+					var notify = () => alert("rellena alguno de los campos");
+
+					return validate() ? doSearch() : notify();
+				};
+
+				return this;
+			};
+
+			var finder = new Finder('finder');
+			Object.assign(finder, new ViewModel(finder, view));
+			finder.render();
+			finder.bind();
+			return finder;
+		}
+	}
+
+	return module;
 });
