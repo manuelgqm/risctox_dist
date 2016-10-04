90f48cbd8c28c31f709fcfaa8201c824b9455287
Lolo <exit>
2016-10-04 15:46:26 +0200

    added view for substanceSearch

--- mobile/js/app/viewModel/substanceSearch.js
+++ mobile/js/app/viewModel/substanceSearch.js
@@ -1,8 +1,18 @@
-define(function(){
+define([
+	'app/viewModel/ViewModel',
+	'text!app/view/substanceSearch.html', 
+], function(ViewModel, view){
 	var module = {
 		run: function(){
-			console.log("name :" + this.name);
-			console.log("code :" + this.code);
+			var search = {
+				domId : "search",
+				name: this.name,
+				code: this.code
+			};
+			Object.assign(search, new ViewModel(search, view));
+			search.render();
+			search.bind();
+			return search;
 		}
 	}
 	return module;
