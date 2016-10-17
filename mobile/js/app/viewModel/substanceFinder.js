39892cd9951ae979a2ee1410028b0aefed47ea36
Lolo <exit>
2016-10-17 16:08:56 +0200

    add name > 3 validation to finder

--- mobile/js/app/viewModel/substanceFinder.js
+++ mobile/js/app/viewModel/substanceFinder.js
@@ -10,15 +10,25 @@ define([
 				this.name = ko.observable('');
 				this.code = ko.observable('');
 				this.runFinder = function () {
+					var self = this;
 					var doSearch = () => window.location = "#/search/" + this.name() + "/" + this.code();
-					var validate = () => this.name().length + this.code().length != 0;
-					var notify = () => alert("rellena alguno de los campos");
+					var getValidationMessages = function(){
+						var result = [];
+						if (0 < self.name().length && self.name().length < 3)	
+							result.push("El nombre de la sustancia debe tener al menos 3 caracteres");
+						if (self.code().length == 0 && self.name().length < 3)
+							result.push("Al menos uno de los campos ha de estar completado");
 
-					return validate() ? doSearch() : notify();
-				};
+						return result;
+					}
+					var notify = messages => messages.map( message => alert(message));
+					var validationMessages = getValidationMessages();
+
+					return !validationMessages.length ? doSearch() : notify(validationMessages);
+				}
 
 				return this;
-			};
+			}
 
 			var finder = new Finder('finder');
 			Object.assign(finder, new ViewModel(finder, view));
