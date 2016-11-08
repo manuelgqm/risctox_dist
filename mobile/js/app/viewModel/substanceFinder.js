f2a074706d8425b30f02d6fc56700f6979186702
Lolo <exit>
2016-11-08 12:14:57 +0100

    add form validation messages to finder view

--- mobile/js/app/viewModel/substanceFinder.js
+++ mobile/js/app/viewModel/substanceFinder.js
@@ -5,32 +5,23 @@ define([
 ], function(ko, ViewModel, view){
 	var module = {
 		run: function(){
-			var Finder = function(domId){
-				this.domId = domId;
-				this.name = ko.observable('');
-				this.code = ko.observable('');
-				this.runFinder = function () {
-					var self = this;
-					var doSearch = () => window.location = "#/search/" + this.name() + "/" + this.code();
-					var getValidationMessages = function(){
-						var result = [];
-						if (0 < self.name().length && self.name().length < 3)	
-							result.push("El nombre de la sustancia debe tener al menos 3 caracteres");
-						if (self.code().length == 0 && self.name().length < 3)
-							result.push("Al menos uno de los campos ha de estar completado");
+			var finder = 
+				{ domId: "finder"
+				, name: ko.observable('')
+				, code: ko.observable('')
+				, messages: ko.observableArray()
+			};
 
-						return result;
-					}
-					var notify = messages => messages.map( message => alert(message));
-					var validationMessages = getValidationMessages();
+			finder.find = function(){
+				if (0 < this.name().length && this.name().length < 3)
+					this.messages.push("El nombre de la sustancia debe tener al menos 3 caracteres");
+				if (this.code().length == 0 && this.name().length < 3)
+					this.messages.push("Al menos uno de los campos ha de estar completado");
+				var doSearch = () => window.location = "#/search/" + this.name() + "/" + this.code();
 
-					return !validationMessages.length ? doSearch() : notify(validationMessages);
-				}
+				return this.messages().length ? false : doSearch();
+			};
 
-				return this;
-			}
-
-			var finder = new Finder('finder');
 			Object.assign(finder, new ViewModel(finder, view));
 			finder.render();
 			finder.bind();
