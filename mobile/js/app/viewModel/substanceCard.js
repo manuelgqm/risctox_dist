e89c385edef1a821691a98c97ee80c34a27c3e77
Lolo <exit>
2016-11-17 12:26:40 +0100

    refactor: remove duplication of logic in has* methods to hasItems function

--- mobile/js/app/viewModel/substanceCard.js
+++ mobile/js/app/viewModel/substanceCard.js
@@ -10,34 +10,66 @@ define([
 		run: function(){
 			var substanceCard = 
 				{ domId: 'card'
-				, hasVlaEstado: function()
-					{ return this.valoresLimiteAmbiental.map(element => element.estado).filter(element => element.length != 0).length != 0;	}
+				, hasVlaEstado: function() { return hasItems(this.valoresLimiteAmbiental, "estado") }
+
 				, hasVlaEd: function()
 					{
-					var ed_mg_m3Valued = this.valoresLimiteAmbiental.map(element => element.ed_mg_m3).filter(element => element.length != 0).length;
-					var ed_ppmValued = this.valoresLimiteAmbiental.map(element => element.ed_ppm).filter(element => element.length != 0).length;
+					var ed_mg_m3Valued = this.valoresLimiteAmbiental
+						.map(element => element.ed_mg_m3)
+						.filter(element => element.length != 0)
+						.length;
+					var ed_ppmValued = this.valoresLimiteAmbiental
+						.map(element => element.ed_ppm)
+						.filter(element => element.length != 0)
+						.length;
 					return ed_mg_m3Valued + ed_ppmValued > 0;
 					}
+
 				, hasVlaEc: function()
 					{
-					var ec_mg_m3Valued = this.valoresLimiteAmbiental.map(element => element.ec_mg_m3).filter(element => element.length != 0).length;
-					var ec_ppmValued = this.valoresLimiteAmbiental.map(element => element.ec_ppm).filter(element => element.length != 0).length;
+					var ec_mg_m3Valued = this.valoresLimiteAmbiental
+						.map(element => element.ec_mg_m3)
+						.filter(element => element.length != 0)
+						.length;
+					var ec_ppmValued = this.valoresLimiteAmbiental
+						.map(element => element.ec_ppm)
+						.filter(element => element.length != 0)
+						.length;
 					return ec_mg_m3Valued + ec_ppmValued > 0;
 					}
+
 				, hasVlaNotas: function()
-					{ return this.valoresLimiteAmbiental.map(element => element.notas).length != 0;	}
+					{ return this.valoresLimiteAmbiental
+						.map(element => element.notas)
+						.length != 0;
+					}
+
 				, hasEinecs: function()
-					{ return this.num_ce_einecs && !beginsWithChar(this.num_ce_einecs, 4) && !this.num_ce_elincs }
+					{ return (
+						this.num_ce_einecs 
+						&& !beginsWithChar(this.num_ce_einecs, 4) 
+						&& !this.num_ce_elincs 					
+						)
+					}
+
 				, hasElincs: function()
-					{ return this.num_ce_elincs && (beginsWithChar(this.num_ce_einecs, 4) || !this.num_ce_einecs) }
-				, hasVlbIb: function()
-					{ return this.valoresLimiteBiologico.map(element => element.indicador).filter(element => element.length != 0).length != 0; }
-				, hasVlbValor: function()
-					{ return this.valoresLimiteBiologico.map(element => element.valor).filter(element => element.length != 0).length != 0; }
-				, hasVlbMomento: function()
-					{ return this.valoresLimiteBiologico.map(element => element.momento).filter(element => element.length != 0).length != 0; }
+					{ return ( 
+						this.num_ce_elincs 
+						&& (beginsWithChar(this.num_ce_einecs, 4) 
+							|| !this.num_ce_einecs) 
+						)
+					}
+
+				, hasVlbIb: function() { return hasItems(this.valoresLimiteBiologico, "indicador") }
+
+				, hasVlbValor: function() { return hasItems(this.valoresLimiteBiologico, "valor") }
+
+				, hasVlbMomento: function() { return hasItems(this.valoresLimiteBiologico, "momento") }
+
 				, hasVlbNotas: function()
-					{ return this.valoresLimiteBiologico.map(element => element.notas).length != 0;	}
+					{ return this.valoresLimiteBiologico
+						.map(element => element.notas).length != 0;	
+					}
 				, hasClassificationOrLabelingRd1272: function()
 					{ return this.pictogramasRd.lengt 
 					|| this.clasificacionesRd1272.length
@@ -45,6 +77,7 @@ define([
 					|| this.concentracionEtiquetadoRd1272.length
 					}
 				};
+
 			var substance = new SubstanceModel(this.id);
 			Object.assign
 				( substanceCard
@@ -64,6 +97,13 @@ define([
 
 			var beginsWithChar = function (string, beginChar) {
 				return !string ? false : string.substring(0, 1) == beginChar;
+			}
+
+			var hasItems = function(list, elementName) {
+				return list
+					.map(element => element[elementName])
+					.filter(element => element.length != 0)
+					.length != 0; 
 			}
 
 			return substanceCard;
