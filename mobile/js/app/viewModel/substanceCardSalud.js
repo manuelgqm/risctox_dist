2205fb44d752a4fbe7ebbac7c02ac1f0f92368dc
Lolo <exit>
2016-11-30 13:17:24 +0100

    add card view logic to decide display of cancerigenos section

--- mobile/js/app/viewModel/substanceCardSalud.js
+++ mobile/js/app/viewModel/substanceCardSalud.js
@@ -3,7 +3,24 @@ define(
 	, 'text!app/view/substanceCardSalud.html'
 	], function(ko, template){
 		function viewModel(card){
+			this.featuredLists = card.featuredLists;
 			Object.assign(this, card.salud);
+		};
+
+		viewModel.prototype.isCancerigeno = function(){
+			var cancerigenosLists = 
+				[ 'cancer_rd'
+				, 'cancer_danesa'
+				, 'cancer_iarc'
+				, 'cancer_otras'
+				, 'cancer_mama' ]
+			var inCancerigenosLists = cancerigenosLists.filter( 
+				cancerigeno => 
+					this.featuredLists().filter( featured =>
+						cancerigeno == featured
+					).length != 0
+				).length != 0;
+			return inCancerigenosLists;
 		};
 
 		return { viewModel: viewModel, template: template };
