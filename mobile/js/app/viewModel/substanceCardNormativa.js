fb3c4f5596153e2dd7387c357f9478cfcab355d6
Lolo <exit>
2016-11-30 16:14:24 +0100

    add fields prohibida embarazadas and lactantes to card normativa section

--- mobile/js/app/viewModel/substanceCardNormativa.js
+++ mobile/js/app/viewModel/substanceCardNormativa.js
@@ -5,6 +5,8 @@ define(
 		function viewModel(card){
 			Object.assign(this, card.identificacion);
 			this.isRestringida = card.featuredLists().indexOf('restringidas') != -1;
+			this.isProhibidaEmbarazadas = card.featuredLists().indexOf('prohibidas_embarazadas') != -1;
+			this.isProhibidaLactantes = card.featuredLists().indexOf('prohibidas_lactantes') != -1;
 		};
 
 		return { viewModel: viewModel, template: template };
