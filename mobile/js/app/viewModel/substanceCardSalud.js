590de5ff71993b1e11faf8c84dc198faf29b4366
Lolo <exit>
2016-12-02 12:12:23 +0100

    refactor: cardSalud code simplification with ecma6 features

--- mobile/js/app/viewModel/substanceCardSalud.js
+++ mobile/js/app/viewModel/substanceCardSalud.js
@@ -1,43 +1,35 @@
 define(
-	[ 'knockout'
-	, 'text!app/view/substanceCardSalud.html'
-	], function(ko, template){
+	[ 'text!app/view/substanceCardSalud.html' ]
+	, function(template){
 		function viewModel(card){
 			Object.assign(this, card.salud);
-			this.featuredLists = card.featuredLists;
-			this.isCancerigenoIarc = this.inList('cancer_iarc');
-			this.isDisruptor = this.inList("de");
-			this.isNeurotoxico = function(){
-				var neurotoxicosLists = 
+			var featuredLists = card.featuredLists();
+			this.isCancerigenoIarc = featuredLists => 
+				inLists(['cancer_iarc'], featuredLists);
+			this.isDisruptor = featuredLists => 
+				inLists(['de'], featuredLists);
+			this.isNeurotoxico = featuredLists => 
+				inLists(
 					[ "neurotoxico"
 					, "neurotoxico_rd"
 					, "neurotoxico_danesa"
-					, "neurotoxico_nivel"
-					]
-				return inLists(neurotoxicosLists, this.featuredLists()) || this.efecto_neurotoxico == 'OTOTÓXICO';
-			};
-			this.isCancerigeno = function(){
-				var cancerigenosLists = 
+					, "neurotoxico_nivel" ]
+				, featuredLists) || this.efecto_neurotoxico == 'OTOTÓXICO';
+
+			this.isCancerigeno = featuredLists =>
+				inLists(
 					[ 'cancer_rd'
 					, 'cancer_danesa'
 					, 'cancer_iarc'
 					, 'cancer_otras'
 					, 'cancer_mama' ]
-				return inLists(cancerigenosLists, this.featuredLists());
+				, featuredLists);
+
+			var inLists = function(list1, list2) {
+				return list1.filter( element => 
+					list2.indexOf(element) != -1
+				).length != 0;
 			};
-
-			var inLists = function(list1, list2){
-				var ocurrences = list1.filter( element1 => 
-					list2.filter( element2 =>
-						element1 == element2
-					).length != 0
-				).length;
-				return ocurrences != 0;	
-			}
-		};
-
-		viewModel.prototype.inList = function (listName) {
-			return this.featuredLists().indexOf(listName) != -1;
 		};
 
 		return { viewModel: viewModel, template: template };
