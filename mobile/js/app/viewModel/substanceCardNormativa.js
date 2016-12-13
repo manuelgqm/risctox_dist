c588b59af8b8a23a8700997d922cf5016dd10efd
Lolo <exit>
2016-12-13 11:46:31 +0100

    fix: bad card normativa view logic for null values

--- mobile/js/app/viewModel/substanceCardNormativa.js
+++ mobile/js/app/viewModel/substanceCardNormativa.js
@@ -4,15 +4,17 @@ define(
 	], function(ko, template){
 		function viewModel(card){
 			Object.assign(this, card.identificacion);
-			this.featuredLists = card.featuredLists;
-			this.isRestringida = this.inList('restringidas');
-			this.isProhibidaEmbarazadas = this.inList('prohibidas_embarazadas');
-			this.isProhibidaLactantes = this.inList('prohibidas_lactantes');
+			var featuredLists = card.featuredLists();
+			var inList = function (list1, list2) {
+				if (!list1 || !list2)
+					return false;
+				return list2.indexOf(list1) != -1;
+			};
+			this.isRestringida = ko.computed( () => inList('restringidas', featuredLists));
+			this.isProhibidaEmbarazadas = ko.computed( () => inList('prohibidas_embarazadas', featuredLists));
+			this.isProhibidaLactantes = ko.computed( () => inList('prohibidas_lactantes', featuredLists));
 		};
 
-		viewModel.prototype.inList = function (listName) {
-			return this.featuredLists().indexOf(listName) != -1;
-		};
 
 		return { viewModel: viewModel, template: template };
 	}
