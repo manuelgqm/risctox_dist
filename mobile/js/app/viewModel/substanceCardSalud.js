620b6110619c1362c4e43c0466a765d4e645e4c6
Lolo <exit>
2016-12-13 11:38:00 +0100

    fix: bad card salud view logic for null values

--- mobile/js/app/viewModel/substanceCardSalud.js
+++ mobile/js/app/viewModel/substanceCardSalud.js
@@ -1,30 +1,39 @@
 define(
-	[ 'text!app/view/substanceCardSalud.html' ]
-	, function(template){
+	[ 'knockout'
+	, 'text!app/view/substanceCardSalud.html' ]
+	, function(ko, template){
 		function viewModel(card){
 			Object.assign(this, card.salud);
 			var featuredLists = card.featuredLists();
 			var inLists = function(list1, list2) {
+				if (!list1 || !list2)
+					return false;
 				return list1.filter( element => 
 					list2.indexOf(element) != -1
 				).length != 0;
 			};
-			this.isCancerigenoIarc = inLists(['cancer_iarc'], featuredLists);
-			this.isDisruptor = inLists(['de'], featuredLists);
-			this.isNeurotoxico = inLists(
+			this.isCancerigenoIarc = ko.computed( () => inLists(['cancer_iarc'], featuredLists));
+			this.isDisruptor = ko.computed( () => inLists(['de'], featuredLists));
+			this.isNeurotoxico = ko.computed( () => 
+				inLists(
 					[ "neurotoxico"
 					, "neurotoxico_rd"
 					, "neurotoxico_danesa"
 					, "neurotoxico_nivel" ]
-				, featuredLists) || this.efecto_neurotoxico == 'OTOTÓXICO';
-			this.isCancerigeno = inLists(
+				, featuredLists) 
+				|| this.efecto_neurotoxico == 'OTOTÓXICO'
+			);
+			this.isCancerigeno = ko.computed( () => 
+				inLists(
 					[ 'cancer_rd'
 					, 'cancer_danesa'
 					, 'cancer_iarc'
 					, 'cancer_otras'
 					, 'cancer_mama' ]
-				, featuredLists);
-			this.isToxicoReproduccion = inLists(['tpr'], featuredLists);
+				, featuredLists)
+			);
+			this.isToxicoReproduccion = ko.computed( () => inLists(['tpr'], featuredLists));
+			
 		};
 
 		return { viewModel: viewModel, template: template };
