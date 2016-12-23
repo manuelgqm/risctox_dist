6f75827ea261c668c79a12cd339a655d1adcdc35
Lolo <exit>
2016-12-23 12:24:45 +0100

    refactor simpplify cardMedioAmbiente view logic

--- mobile/js/app/viewModel/substanceCardMedioAmbiente.js
+++ mobile/js/app/viewModel/substanceCardMedioAmbiente.js
@@ -14,15 +14,9 @@ define(
 			};
 			this.isTpb = ko.computed( () =>	inLists(['tpb'], featuredLists) );
 			this.isToxicaAgua = ko.computed( () => inLists(['directiva_aguas', 'alemana'], featuredLists) );
-			this.isDirectivaAguas = ko.computed( () =>
-				inLists(['directiva_aguas'], featuredLists)
-				|| this.directiva_aguas()
-			, this);
+			this.isDirectivaAguas = ko.computed( () => inLists(['directiva_aguas'], featuredLists) );
 			this.isPrioritaria = ko.computed( () => inLists(['sustancias_prioritarias'], featuredLists) );
-			this.isMedioAmbienteAlemania = ko.computed( () =>
-				this.clasif_mma()
-				&& !isNaN(!isNaN(this.clasif_mma()[0].key()))
-			, this);
+			this.isMedioAmbienteAlemania = ko.computed( () => inLists(['alemana'], featuredLists) );
 			this.isContaminanteAire = ko.computed( () =>
 				inLists(['ozono', 'clima', 'aire'], featuredLists)
 			);
