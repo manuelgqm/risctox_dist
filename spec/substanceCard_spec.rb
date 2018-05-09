e32ceecf0e63df870cd4f5402cf3778f7341ee25
Manuel García de Quirós Moreno <manuelgqm@gmail.com>
2018-05-09 18:48:58 +0200

    e2e go headless

--- spec/substanceCard_spec.rb
+++ spec/substanceCard_spec.rb
@@ -1,8 +1,9 @@
 require 'watir'
 
-browser = Watir::Browser.new
+browser = Watir::Browser.new :chrome, headless: true
 
 RSpec.configure do |config|
+  # only used on headed browser option
   config.before(:each) { @browser = browser }
   config.after(:suite) { browser.close unless browser.nil? }
 end
@@ -17,7 +18,7 @@ class SpanElement
 
 end
 
-describe "'hydrogen cyanide' subatance card" do
+describe "'hydrogen cyanide' substance card" do
   before(:each) do
     @browser.goto("http://localhost:8081/en/dn_risctox_ficha_sustancia.asp?id_sustancia=953980")
   end
@@ -25,8 +26,8 @@ describe "'hydrogen cyanide' subatance card" do
   describe "that has valid field labels and values" do
     it "should had a correct name" do
       name_element = SpanElement.new("name", @browser)
-      name_element.value.should include ('hydrogen cyanide')
-      name_element.label.should include ('Chemical name')
+      expect(name_element.value).to include ('hydrogen cyanide')
+      expect(name_element.label).to include ('Chemical name')
     end
   end
 
